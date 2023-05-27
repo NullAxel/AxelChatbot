00aa3bae04b87eda0f741e6b69b390c386d13ce8
naielv <109038805+naielv@users.noreply.github.com>
2023-05-27 20:30:41 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -33,7 +33,6 @@ def generate(inp: str, args = {}):
                 response = choice(INTENTS[intent_name])
             return {"result": response, "intent": intent_name, "input": inp}
         except KeyError as e:
-            raise e
             #return {"result": "El Pepe"}
-            #return {"result": choice(ERROR_RESPONSES), "intent": intent_name, "input": inp, "error": [0, "Missing intent '" + intent_name + "' in config."]}
+            return {"result": choice(ERROR_RESPONSES), "intent": intent_name, "input": inp, "error": [0, "Missing intent '" + intent_name + "' in config."]}
         
