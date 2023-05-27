8366d48f77cb4ff44598e55ccb9f0675555171c6
naielv <109038805+naielv@users.noreply.github.com>
2023-05-27 11:19:18 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -23,5 +23,5 @@ def ask_url(text):
     return generate(text)["result"]
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
  
