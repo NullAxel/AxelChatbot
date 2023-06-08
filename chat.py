0050e9fce962acd06b1d3e384feeb894f05b7782
naielv <109038805+naielv@users.noreply.github.com>
2023-06-08 18:27:00 +0000

    MAJOR CHANGE: Added WolframAlpha

--- chat.py
+++ chat.py
@@ -3,6 +3,8 @@ import os
 import json
 from random import choice
 from dotenv import load_dotenv
+from wolframalpha import Client as WAClient
+from translate import Translator
 load_dotenv()
 access_token=os.environ["WIT_ACCESS_TOKEN"]
 client = Wit(access_token)
@@ -10,11 +12,17 @@ CONFIG = json.load(open("responses.json", "r", encoding="utf-8"))
 ERROR_RESPONSES=CONFIG["ERROR_RESPONSES"]
 INTENTS=CONFIG["RESPONSES"]
 SMART=CONFIG["SMART"]
+waclient = WAClient(os.environ["WA_APP_ID"])
 
-logs = open("logs.txt", "a", encoding="utf-8")
+def wolframalpha(inp: str, lang: str = "es"):
+    translator_from = Translator(from_lang=lang, to_lang="en")
+    translator_to = Translator(from_lang="en", to_lang=lang)
+    eninp = translator_from.translate(inp)
+    res = waclient.query(eninp)
+    result = next(res.results).text
+    esresult = translator_to.translate(result)
+    return esresult
 def generate(inp: str, args = {}):
-    logs.write(inp)
-    print(inp)
     result = client.message(inp)
     if result["intents"] == []:
         intent_name = ""
@@ -22,20 +30,22 @@ def generate(inp: str, args = {}):
     else:
         intent_name = result["intents"][0]["name"]
         intent_conf = result["intents"][0]["confidence"]
+    print(intent_name)
     if intent_name == "" or intent_conf < 0.925:
-        return {"result": choice(ERROR_RESPONSES), "intent": "unknown", "input": inp}
+        try:
+            result = wolframalpha(inp, lang="es")
+            return {"result": result, "intent": intent_name, "input": inp, "with": "wolframalpha"}
+        except StopIteration:
+            return {"result": choice(ERROR_RESPONSES), "intent": intent_name, "input": inp, "with": "error"}
     else:
         try:
-            # response = choice(INTENTS[intent_name].format(args=args))
             if str(args.get("mode")) == "smart":
                 s=INTENTS
                 s.update(SMART)
                 response = choice(s[intent_name]).format(args=args)
             else:
                 response = choice(INTENTS[intent_name])
-            return {"result": response, "intent": intent_name, "input": inp}
-        except KeyError as e:
-            print(e)
-            #return {"result": "El Pepe"}
-            return {"result": choice(ERROR_RESPONSES), "intent": intent_name, "input": inp, "error": [0, "Missing intent '" + intent_name + "' in config."]}
+            return {"result": response, "intent": intent_name, "input": inp, "with": "responses"}
+        except KeyError:
+            return {"result": choice(ERROR_RESPONSES), "intent": intent_name, "input": inp, "with": "error"}
         
